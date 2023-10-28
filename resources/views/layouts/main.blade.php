94c6353d577c78dbfcfc7570d6a0dff5218f44bc
arslankhan0123 <arslan.devsspace@gmail.com>
2023-10-28 15:27:23 +0500

    use datatables code in public project

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -95,8 +95,8 @@
 
     <!-- For DataTable -->
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/admin/datatables/datatables.css') }}">
+    <script type="text/javascript" charset="utf8" src="{{ asset('/admin/datatables/datatables.js') }}"></script>
     <script>
         jQuery(document).ready(function($) {
             $('#example1').DataTable();
